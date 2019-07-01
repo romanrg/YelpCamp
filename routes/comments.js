f45782160f543237bf95d9a26479cf522c828ed3
romanrg <nekhaichyk.raman@gmail.com>
2019-07-01 22:30:55 +0300

    editing commets route

--- routes/comments.js
+++ routes/comments.js
@@ -35,6 +35,30 @@ router.post("/", isLoggedIn, (req, res) => {
     })
 });
 
+//COMMENTS EDIT ROUTE
+router.get('/:comment_id/edit', (req, res)=>{
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if(err) {
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", {camp_id: req.params.id, comment: foundComment});
+        }
+    });    
+});
+//COMMENT UPDATE
+router.put('/:comment_id', (req, res)=> {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment)=>{
+        if(err) {
+            res.redirect('back');
+        } else {
+            res.redirect(`/index/${req.params.id}`);
+        }
+    })
+});
+
+
+
+
 function isLoggedIn (req, res, next){
     if(req.isAuthenticated()){
         return next();
